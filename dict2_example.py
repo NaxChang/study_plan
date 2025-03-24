fca3b3c329f14932b69dadf0debd3ab1a79c61bd
naxchang <naxchang@gmail.com>
2025-03-24 19:57:53 +0800

    25_0324

---
+++ dict2_example.py
@@ -0,0 +1,19 @@
+person = {
+    "姓名": "Nameless",
+    "年齡": 30,
+    "職業": "找工作",
+}
+
+print(person.keys())
+print(person.values())
+
+# print(person.get("地址", "無地址"))
+
+# print(person.get("姓名"))
+# print(person.get("地址"))
+
+# print(person["地址"])
+
+# print("姓名：", person["姓名"])  # 輸出：姓名：Nameless
+# print("年齡：", person["年齡"])  # 輸出：年齡：30
+# print("職業：", person["職業"])  # 輸出：職業：找工作
---
+++ tuple1_example.py
@@ -0,0 +1,4 @@
+test_03 = []
+print(id(test_03))
+test_03.append(1)
+print(id(test_03))
